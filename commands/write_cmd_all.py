55192485ae9b49d4cd92a6f056d44173a2674e69
Asher Sang <hsang012@ucr.edu>
2025-01-13 17:20:49 -0800

    1. Add comments; 2. Add analysis Python file.

--- commands/write_cmd_all.py
+++ commands/write_cmd_all.py
@@ -1,45 +1,36 @@
-import numpy as np
-from scipy.optimize import curve_fit
-import matplotlib.pyplot as plt
-import scipy.interpolate
-from scipy.interpolate import interp1d
-import matplotlib.patches as mpatches
-import math
-from scipy.misc import derivative
-from math import log
-from math import sqrt
-import matplotlib.cm as cm
-import pickle
-import pprint
 import os
-import sys
-import time
-import glob
-import re
-import shutil
-import subprocess
 
 
+file_name = 'run2'
+snapshot_number = 62
 folder_name = os.listdir('../../') 
-folder_name = [folder for folder in folder_name if 'DM' in folder]
-folder_name.sort()
+folder_name = 'Gadget2/SIDM/codeSIDM3/output_testRun2_job'
+#     [folder for folder in folder_name if 'DM' in folder]
+# folder_name.sort()
 print(folder_name)
 
-j=0
+with open(file_name + '.sh', 'w') as sh:
+    sh.write('rm ../../' + folder_name + '/boundmass.txt\n')
+    sh.write('rm ../../' + folder_name + '/dataProfile/*\n')
+    for i in range(snapshot_number):
+        sh.write('nohup srun --mem=10gb --pty ./modRS ../../' + folder_name + ' snapshot {:03} -l &\n'.format(i))
+sh.close()
 
-with open("sh-all.sh", 'w') as runall:
-  for folder in folder_name:
-      runall.write('sh ' + folder + '.sh\n')
-      j+=1
-      if j%2==0:
-        runall.write('sleep 135\n')
-      with open(folder + '.sh','w') as sh:
-         sh.write('rm ../../'+folder+'/output/boundmass.txt\n')
-         sh.write('rm ../../'+folder+'/output/dataProfile/*\n')
-         for i in range(60):
-            sh.write('nohup srun --mem=10gb --pty ./modRS ../../' +folder+ '/output snapshot {:03} -l &\n'.format(i))
-      sh.close()
-runall.close
+# j=0
+
+# with open("sh-all.sh", 'w') as runall:
+#   for folder in folder_name:
+#       runall.write('sh ' + folder + '.sh\n')
+#       j+=1
+#       if j%2==0:
+#         runall.write('sleep 135\n')
+#       with open(folder + '.sh','w') as sh:
+#          sh.write('rm ../../'+folder+'/output/boundmass.txt\n')
+#          sh.write('rm ../../'+folder+'/output/dataProfile/*\n')
+#          for i in range(60):
+#             sh.write('nohup srun --mem=10gb --pty ./modRS ../../' +folder+ '/output snapshot {:03} -l &\n'.format(i))
+#       sh.close()
+# runall.close
 
 
 
